55551213fb20d7f07b58b625a5c71a01f431eac2
fakhrulraharjo <fakhrulraharjo@gmail.com>
2024-01-19 13:59:34 +0700

    add dynamic directus endpoint

--- components/directus/index.ts
+++ components/directus/index.ts
@@ -11,7 +11,14 @@ import QueryProvider from "./queryWrapper";
 import { useGetCategory, useGetPost } from "./get";
 import { Scheema } from "./type";
 
-export const client = createDirectus<Scheema>("http://localhost:3000/cms")
+const cmsEnpoint = () => {
+  if (process.env.DIRECTUS_HOST) return process.env.DIRECTUS_HOST;
+  if (process.env.NEXT_PUBLIC_DIRECTUS_HOST)
+    return process.env.NEXT_PUBLIC_DIRECTUS_HOST;
+  return "http://directus-directus-1:8055";
+};
+
+export const client = createDirectus<Scheema>(cmsEnpoint())
   .with(staticToken("rHRDSi1UJ9A_xyYh2kO9dg1K-rRWynQd"))
   .with(rest());
 
